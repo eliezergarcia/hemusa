58f8ef3ff8aab9e9d887748b49172d644da32092
Eliezer Hernández <2dcc.eh@gmail.com>
2018-07-17 10:58:17 -0500

    Correcciones

--- php/conexion.php
+++ php/conexion.php
@@ -2,11 +2,11 @@
   $server= $_SERVER["HTTP_HOST"];
 	$user = "root";
 	$password = "1234";
-	$db = "pruebas";
-  $database_conexion_usuarios = "pruebas";
+	$db = "hemusa";
+  $database_conexion_usuarios = "hemusa";
 	$ruta = "http://".$server."/sistemahemusa/";
 
-	$conexion_usuarios = mysqli_connect("18.236.250.216", "hemusadb", "Hemusa@2017", "pruebas");
+	$conexion_usuarios = mysqli_connect("18.236.250.216", "hemusadb", "Hemusa@2017", "hemusa");
 	// $conexion_usuarios = mysqli_connect($server, $user, $password, $db);
 	// $conexion_usuarios = mysqli_connect("127.0.0.1", "root", "", "hemusa2018");
 	if(!$conexion_usuarios){
