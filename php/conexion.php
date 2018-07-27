1466e5aa3d2ca8e5e61c85ce90991ea92c11b255
Eliezer Hernández <2dcc.eh@gmail.com>
2018-07-27 10:28:48 -0500

    Cambios en ordenes de compras

--- php/conexion.php
+++ php/conexion.php
@@ -2,11 +2,11 @@
   $server= $_SERVER["HTTP_HOST"];
 	$user = "root";
 	$password = "1234";
-	$db = "hemusas";
-  $database_conexion_usuarios = "hemusas";
+	$db = "hemusa";
+  $database_conexion_usuarios = "hemusa";
 	$ruta = "http://".$server."/sistemahemusa/";
 
-	$conexion_usuarios = mysqli_connect("18.236.250.216", "hemusadb", "Hemusa@2017", "hemusas");
+	$conexion_usuarios = mysqli_connect("18.236.250.216", "hemusadb", "Hemusa@2017", "hemusa");
 	// $conexion_usuarios = mysqli_connect($server, $user, $password, $db);
 	// $conexion_usuarios = mysqli_connect("127.0.0.1", "root", "", "hemusa2018");
 	if(!$conexion_usuarios){
